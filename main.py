032244a267bd446a542959543784edbeacca5680
baconismycopilot <justondroney@gmail.com>
2020-08-15 10:26:44 -0400

    Update main.py
    Change schedule to 30

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from rtm import ReleaseTagMaker
 
-rt = ReleaseTagMaker('release.07.30.20v3', schedule=60)
+rt = ReleaseTagMaker('release.07.30.20v3', schedule=30)
 print(rt.exploded)
 print(rt.bump_version())
 print(rt.new_tag())
