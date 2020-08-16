f340e30f2040862786b05526ce141a8683d99663
Juston Roney <justondroney@gmail.com>
2020-08-16 13:55:54 -0400

    make release tag an object

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from rtm import ReleaseTagMaker
+from rtm import ReleaseTagMaker, ObjectFromReleaseTag
 
-rt = ReleaseTagMaker('release.07.30.20v3', schedule=30)
-print(rt.exploded)
-print(rt.bump_version())
-print(rt.new_tag())
+rt = ReleaseTagMaker('release.07.30.20v3')
+# ro = ObjectFromReleaseTag(rt.exploded)
+print(rt.from_dict())
+# print(ro)
+
+# print(rt.bump_version())
+# print(rt.new_tag())
+# print(rt.schedule)
--- rtm.py
+++ rtm.py
@@ -1,6 +1,29 @@
 from datetime import datetime, timedelta
+from typing import List
+
+import attr
+import cattr
 
 
+@attr.s(slots=True)
+class ObjectFromReleaseTag:
+    """
+    Create an object from exploded release tag.
+    """
+    prefix: str = attr.ib()
+    month: int = attr.ib()
+    day: int = attr.ib()
+    year: int = attr.ib()
+    version: int = attr.ib()
+    schedule: int = attr.ib()
+    hotfix: str = attr.ib(default=None)
+
+    @classmethod
+    def from_dict(cls, data: dict):
+        return cattr.structure_attrs_fromdict(data, ObjectFromReleaseTag)
+
+
+@attr.s(slots=True)
 class ReleaseTagMaker:
     """
     Provide methods to manipulate release tags.
@@ -9,18 +32,19 @@ class ReleaseTagMaker:
     ex: ReleaseTagMaker('release.07.30.20v3[.hf]')
     """
 
-    def __init__(self, old_release_tag, schedule=None):
-        self.old_release_tag = old_release_tag
+    def __init__(self, old_release_tag: str, schedule=None):
+        self.old_release_tag: str = old_release_tag
         self.new_release_tag = None
-        self.schedule = schedule
+        self.schedule: int = schedule
         if schedule is None:
             self.schedule = 30
         else:
             if not isinstance(schedule, int):
                 raise TypeError("Parameter schedule must be int.")
 
-    @property
-    def exploded(self):
+
+    @classmethod
+    def from_dict(cls, **release_dict):
         """
         Explode a release tag and send its bits all over the place.
         Expected release tag looks like release.mm.dd.YYv1[.hf]
@@ -28,28 +52,46 @@ class ReleaseTagMaker:
         :return: list
         """
 
-        return self.old_release_tag.split('.')
+        release_dict = {}
+        release_list = self.old_release_tag.split('.')
 
-    def bump_version(self):
+        if 'hf' not in release_list:
+            release_dict = {
+                "prefix": release_list[0],
+                "month": release_list[1],
+                "day": release_list[2],
+                "year": release_list[3].split('v')[0],
+                "version": release_list[3].split('v')[1],
+                "schedule": self.schedule
+            }
+
+        if 'hf' in release_list:
+            release_dict = {
+                "prefix": release_list[0],
+                "month": release_list[1],
+                "day": release_list[2],
+                "year": release_list[3].split('v')[0],
+                "version": release_list[3].split('v')[1],
+                "hotfix": release_list[4],
+                "schedule": self.schedule
+            }
+        return ObjectFromReleaseTag.from_dict(**release_dict)
+
+    def bump_version(self) -> ObjectFromReleaseTag:
         """
         Increment the version of a release tag.
         Hotfix release tags won't get bumped.
 
         :return: str
         """
+        if self.exploded.hotfix is None:
+            self.exploded.version += 1
 
-        if 'hf' not in self.exploded:
-            tag = '.'.join(self.exploded[0:4]).split('v')[:1]
-            version = int(self.exploded[3].split('v')[1])
-            version += 1
+        return self.exploded
 
-            return f"{'.'.join(tag)}v{version}"
-
-        return f"{'.'.join(self.exploded)}"
-
-    def new_tag(self):
+    def new_tag(self) -> str:
         """
-        Use the exploded bits of a release tag and make it born again.
+        Use the exploded bits of a release tag and born it again.
 
         :return: str
         """
@@ -61,6 +103,6 @@ class ReleaseTagMaker:
             previous_date: str = f'{self.exploded[1]}.{self.exploded[2]}.{self.exploded[3].split("v")[0]}'
             previous_release: datetime = datetime.strptime(previous_date, '%m.%d.%y')
             next_release: datetime = previous_release + timedelta(days=self.schedule)
-            self.new_release_tag = f"release.{next_release.strftime('%m.%d.%y')}v1"
+            self.new_release_tag: str = f"release.{next_release.strftime('%m.%d.%y')}v1"
 
         return self.new_release_tag
